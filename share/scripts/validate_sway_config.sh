2087d8bac8181c4e35320cafb63c2e368eb358c9
rbrgmn <roman.bergman@tutanota.com>
2024-11-17 13:30:55 +0200

    fix wallpaper path

--- share/scripts/validate_sway_config.sh
+++ share/scripts/validate_sway_config.sh
@@ -4,9 +4,12 @@
 #  AUTHOR: RGBRGM <roman.bergman@tutanota.com>
 # LICENSE: AGPL3.0
 
-SWAY_DIR="./sway.d"
+SWAY_DIR="sway.d"
 
 for file in "$SWAY_DIR"/*; do
+  if [[ "$(basename "$file")" == "wallpapers.conf" ]]; then
+    continue
+  fi
   echo -e "# Base Config\ninclude $file" >>sway.conf
 done
 
